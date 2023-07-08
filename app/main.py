8487bc75221aa1fd27d3cb78d4a14db99c5b0936
Arnab K <krarnab1000@gmail.com>
2023-07-08 23:27:39 +0530

    crud apis using pymysql

--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
-from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
+import pymysql
+import time
+
+db_host_name = "localhost"
+db_user_name = "root"
+db_user_password = "Vivasva@20"
+db_schema_name = "fastapi"
+
+def get_db_connection():
+  connection = None
+  try:
+    connection = pymysql.connect(db= db_schema_name,
+                                 user= db_user_name,
+                                 passwd= db_user_password,
+                                 host= db_host_name)
+    
+    if connection is not None:
+      print("Connection is successful")
+  except Exception as exception:
+    print(f"Connection not successful {exception}")
+  
+  return connection
 
 app = FastAPI()
 
@@ -12,7 +33,6 @@ class Post(BaseModel):
   content: str
   #an optional fields with default set
   published: bool = True
-  rating: Optional[int] = None
 
 def find_post(id):
   for p in my_posts:
@@ -33,57 +53,121 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-  return {"data": my_posts}
+  connection_new = None
+  process_cursor = None
+  try:
+    connection_new = get_db_connection()
+    with connection_new.cursor() as process_cursor:
+      process_cursor.execute("""select * from posts;""")
+      posts = process_cursor.fetchall()
+
+      # print(posts)
+      return {"data": posts}
+  except Exception as e:
+    print(e)
+  finally:
+    if process_cursor is not None:
+      process_cursor.close()
+    if connection_new is not None:
+      connection_new.commit()
+      connection_new.close()
 
 # title string, content string for post payload
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-  post_dict = post.dict()
-  post_dict['id'] = len(my_posts)
-  my_posts.append(post_dict)
-  print(my_posts)
-  # converting post model to dictionary
-  return {"data": post}
+  connection_new = None
+  process_cursor = None
+  try:
+    connection_new = get_db_connection()
+    with connection_new.cursor() as process_cursor:
+      process_cursor.execute("""insert into posts(`title`, `content`, `published`) values (%s, %s, %s)""", 
+                 (post.title, post.content, post.published))
+      process_cursor.execute("""select * from posts order by id desc limit 1;""")
+      new_post = process_cursor.fetchone()
+
+      # print(posts)
+      return {"data": new_post}
+  except Exception as e:
+    print(e)
+  finally:
+    if process_cursor is not None:
+      process_cursor.close()
+    if connection_new is not None:
+      connection_new.commit()
+      connection_new.close()
+  
+  
 
 # order matters in fastapi here latest can be treated as {id} so declaring it before
-@app.get("/posts/latest")
-def get_latest_post():
-  post = my_posts[len(my_posts) - 1]
-  return {"latest_post": post}
+# @app.get("/posts/latest")
+# def get_latest_post():
+#   post = my_posts[len(my_posts) - 1]
+#   return {"latest_post": post}
 
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response):
-  print(id)
-  post = find_post(id)
-
-  if not post:
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                        detail= f"post with id {id} was not found")
-
-  return {"post_detail": post}
+  connection_new = None
+  process_cursor = None
+  try:
+    connection_new = get_db_connection()
+    with connection_new.cursor() as process_cursor:
+      process_cursor.execute("""select * from posts where id = %s""", (id))
+      post = process_cursor.fetchone()
+      if post is None: 
+        
+        return {"detail": f"post with id {id} was not found"}
+      return {"post_detail": post}
+  except Exception as e:
+    print(e)
+  finally:
+    if process_cursor is not None:
+      process_cursor.close()
+    if connection_new is not None:
+      connection_new.commit()
+      connection_new.close()
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-  index = find_index_post(id)
+  connection_new = None
+  process_cursor = None
+  try:
+    connection_new = get_db_connection()
+    with connection_new.cursor() as process_cursor:
+      process_cursor.execute("""select * from posts where id = %s""", (id))
+      post = process_cursor.fetchone()
+      process_cursor.execute("""delete from posts where id = %s;""", (id))
+      if post is None:
+        return {"detail": f"post with id {id} was not found"}
+      return Response(status_code=status.HTTP_204_NO_CONTENT)
+  except Exception as e:
+    print(e)
+  finally:
+    if process_cursor is not None:
+      process_cursor.close()
+    if connection_new is not None:
+      connection_new.commit()
+      connection_new.close()
 
-  if index == None:
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail=f"post with id {id} was not found")
-
-  my_posts.pop(index)
-  return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-  print(post)
-  index = find_index_post(id)
+  connection_new = None
+  process_cursor = None
+  try:
+    connection_new = get_db_connection()
+    with connection_new.cursor() as process_cursor:
+      process_cursor.execute("""update posts set `title`=%s, `content`=%s, `published`=%s where id=%s""", 
+                 (post.title, post.content, post.published, id))
+      process_cursor.execute("""select * from posts where id=%s;""", id)
+      new_post = process_cursor.fetchone()
 
-  if index == None:
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail=f"post with id {id} was not found")
-  
-  post_dict = post.dict()
-  post_dict["id"] = id
-  my_posts[index] = post_dict
-  return {"data": post_dict}
+      return {"data": new_post}
+  except Exception as e:
+    print(e)
+  finally:
+    if process_cursor is not None:
+      process_cursor.close()
+    if connection_new is not None:
+      connection_new.commit()
+      connection_new.close()
